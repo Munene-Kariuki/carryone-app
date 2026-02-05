13216b1999ab1ea8b46de8d2a3052b82a5b11508
brian <muriithibrian283@gmail.com>
2026-02-05 19:04:07 +0300

    Feat: Add projects page

--- src/features/projects/projects.ts
+++ src/features/projects/projects.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
 export class Projects {
+  categories = [
+  'Civil-Works',
+  'Commercial & Industrial',
+  'Fit-Out Works',
+  'Medical-Facility',
+  'Office-Block',
+  'Residential',
+  'Retail',
+  'Hotel',
+  'School'
+  ];
 
+  activeCategory = 'Civil-Works';
+
+  projects = [
+    {
+      title:'Road contruction',
+      category:'Civil-Works',
+      image: '/assets/projects/project1.jpg'
+    },
+    {
+      title:'Contruction',
+      category:'Civil-Works',
+      image: 'assets/projects/project4.jpeg'
+    },
+    {
+      title:'Contruction',
+      category:'Office-Block',
+      image: '../../assets/projects/project4.jpeg'
+    }
+  ]
+  get filteredProjects(){
+    return this.projects.filter(
+      p=>p.category===this.activeCategory
+    );
+  }
 }
